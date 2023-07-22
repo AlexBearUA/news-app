fb33b68fbb9a2f09411453487516076906a529f8
AlexBearUA <113371691+AlexBearUA@users.noreply.github.com>
2023-07-22 21:33:54 +0000

    Deploying to gh-pages from @ AlexBearUA/news-app@a5dbd38c686a77639f21e004893b5b881f4e1cab 🚀

--- static/js/395.940f5196.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunknews_app=self.webpackChunknews_app||[]).push([[395],{395:function(e,n,s){s.r(n),s.d(n,{default:function(){return c}});var u="Home_home__9Ke73",a=s(184),c=function(){return(0,a.jsx)("div",{className:u,children:"Home Page"})}}}]);
-//# sourceMappingURL=395.940f5196.chunk.js.map
