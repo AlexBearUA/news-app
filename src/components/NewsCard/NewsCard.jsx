a9361872889c1859401ba2f087b734cda5403eed
Oleksandr Vedmedenko <stbearua@gmail.com>
2023-07-23 11:52:35 +0200

    image fix

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,7 +1,10 @@
 // import css from './NewsCard.module.scss';
 
 export const NewsCard = ({ abstract, title, url, date, section, media }) => {
-  const imgUrl = media[0]['media-metadata'][0].url;
+  const imgUrl =
+    media.length !== 0
+      ? media[0]['media-metadata'][2].url
+      : 'https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg';
 
   return (
     <div>
